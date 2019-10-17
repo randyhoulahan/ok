06cedc15e6db0bfeaa1207c80bd16488dfeebba4
Randy Houlahan <randy.houlahan@cbd.int>
2019-10-17 11:09:59 -0400

    Report not ok is disk space on specified path is above 90% usage

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const fastify = require('fastify')({})
-const disk = require('diskusage')
+const df = require('@sindresorhus/df');
 
 fastify.get('/ok', async (request, reply) => {
 
   reply.type('application/json').code(200)
 
-  if(!process.env.DISK_SPACE) return 'ok'
+  if(!process.env.DISK_SPACE_PATH) return 'ok'
 
-  const isOk = await isOk(process.env.DISK_SPACE)
-
-  return isOk
+  return isOk(process.env.DISK_SPACE_PATH)
 })
 
 fastify.listen(8888,'0.0.0.0', (err, address) => {
@@ -19,11 +17,14 @@ fastify.listen(8888,'0.0.0.0', (err, address) => {
 
 async function isOk(path) {
   try {
-    const { available, total } = await disk.check(path)
-    const percent = Math.round((available/total)*100)
-    
+    const { size, available } = await df.file(path)
+    const percent = Math.round((available/size)*100)
+
     if(percent > 10) return 'ok'
+    
+    console.warn(`Disk space critically low: ${percent}% free`)
     return percent
+    
   } catch (err) {
     console.error(err)
     return 0
